48409e4b7fbdcf36b1a1b120bbf93e2c7936b5d1
Camilo Mejia <camilosw@gmail.com>
2021-02-15 20:09:23 +0100

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  const isProduction = argv.mode === 'production';
+
+  return {
+    devtool: isDevelopment ? 'eval-cheap-module-source-map' : 'source-map',
+    entry: './src/index',
+    output: {
+      filename: isProduction ? 'js/index.[contenthash:8].js' : 'index.js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    optimization: {
+      minimize: isProduction,
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          exclude: /node_modules/,
+          use: ['eslint-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      isProduction && new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[contenthash:8].css',
+          chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+        }),
+    ].filter(Boolean),
+    devServer: {
+      publicPath: '/',
+      clientLogLevel: 'silent',
+    },
+  };
+};
